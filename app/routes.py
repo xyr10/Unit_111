abc1fe9767292c57b2fd1eb394df35ac7636ace8
Xyrone Ocampo <xyrone@gmail.com>
2023-05-21 08:41:46 -0700

    For assignment 1

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.get("/")                       #get is a method, the dot is the operator, the at symbol is a decorator
+def index():
+    me = {
+        "first_name": "Xyrone",
+        "last_name": "Ocampo",
+        "hobbies": "Reading"
+        "is_active": True
+    }
+    return me
+
+@app.post("/")
+def create_entry():
+    raw_data = request.jsonmylist.append(raw_data)
+    return "", 204
+
+@app.get("/entries")
+def get_entries():
+    return mylist
